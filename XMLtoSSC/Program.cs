452f44acd235ebdfb29b84b0322601407ec86eb0
Ezra Brooks <ezra.m.brooks@gmail.com>
2015-02-27 14:11:34 -0500

    Depend upon SSCSharp DLL instead of cs file

--- XMLtoSSC/Program.cs
+++ XMLtoSSC/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Xml;
 using System.Threading.Tasks;
 using System.IO.Ports;
-using SSCSharp;
+using EzraBrooks.SSCSharp;
 
 namespace XMLtoSSC
 {
--- XMLtoSSC/Properties/AssemblyInfo.cs
+++ XMLtoSSC/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SSCSharpSequencer")]
+[assembly: AssemblyTitle("XMLtoSSC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SSCSharpSequencer")]
+[assembly: AssemblyCompany("Ezra Brooks")]
+[assembly: AssemblyProduct("XMLtoSSC")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
