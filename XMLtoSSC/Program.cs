0fc9e4249490fc297c826114f57301d5df20053d
Ezra Brooks <ezra.m.brooks@gmail.com>
2015-02-27 15:16:24 -0500

    cull old requirements from main program class

--- XMLtoSSC/Program.cs
+++ XMLtoSSC/Program.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml;
-using System.Threading.Tasks;
-using System.IO.Ports;
-using EzraBrooks.SSCSharp;
 
 namespace EzraBrooks.XMLtoSSC
 {
